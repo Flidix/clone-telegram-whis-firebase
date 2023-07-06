6dde589da6b2dace72fe65e6a020b6c7cc0a7fb4
Flidix <marko.lazarchuk@gmail.com>
2023-07-06 13:43:56 +0300

    first commit

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -11,17 +11,3 @@ export const FirebaseConfig = {
 	appId: process.env.APP_ID,
 	measurementId: process.env.MEASUREMENT_ID,
 };
-
-
-
-
-// export const FarebaseConfig = {
-// 	apiKey: process.env.FIREBASE_API_KEY,
-// 	authDomain: "telegram-3a780.firebaseapp.com",
-// 	projectId: "telegram-3a780",
-// 	storageBucket: "telegram-3a780.appspot.com",
-// 	messagingSenderId: "419917998872",
-// 	appId: "1:419917998872:web:651398e4de736a99148c6d",
-// 	measurementId: "G-0P641J4SKY",
-// 	databaseURL: "https://telegram-3a780-default-rtdb.europe-west1.firebasedatabase.app"
-// }
