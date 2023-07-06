4cb5514f8cbcb782cfb161228c0c080e5300cb30
Flidix <marko.lazarchuk@gmail.com>
2023-07-06 16:38:53 +0300

    first commit

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,43 +1,53 @@
-import { BadRequestException, HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common'
-import { initializeApp } from "firebase/app";
-import { getDatabase } from 'firebase/database'
-import { InjectRepository } from '@nestjs/typeorm'
-import { UserEntity } from '../user/entities/user.entity'
-import { Repository } from 'typeorm'
-import { CreateGroupDto } from './dto/create-group.dto'
-import {ref, set, child, get, update, remove, push, } from "firebase/database";
-import { SendMessageDto } from './dto/sendMEssage.dto'
-import { v4 as uuidv4 } from 'uuid';
-import { AddUserToGroupDto } from './dto/addUserToGroup.dto'
-import { FirebaseConfig } from '../config/firebase.config'
+import { BadRequestException, HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 
+// Import Firebase modules
+import { initializeApp } from "firebase/app";
+import { getDatabase } from 'firebase/database';
+
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from '../user/entities/user.entity';
+import { Repository } from 'typeorm';
+import { CreateGroupDto } from './dto/create-group.dto';
+import { ref, set, child, get, update, remove, push } from "firebase/database";
+import { SendMessageDto } from './dto/sendMessage.dto';
+import { v4 as uuidv4 } from 'uuid';
+import { AddUserToGroupDto } from './dto/addUserToGroup.dto';
+import { FirebaseConfig } from '../config/firebase.config';
 
 @Injectable()
 export class ChatService {
   private database;
 
   constructor(
-    @InjectRepository(UserEntity) private readonly userRepository :Repository<UserEntity>,
+    @InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>,
   ) {
     const app = initializeApp(FirebaseConfig);
     this.database = getDatabase(app);
   }
 
-
+  /**
+   * Find a group by its ID
+   * @param groupId - ID of the group
+   * @returns The group data if found, otherwise throws a NotFoundException
+   */
   async findGroupById(groupId: string) {
     const groupRef = ref(this.database, `groups/${groupId}`);
     const groupSnapshot = await get(groupRef);
     const groupData = groupSnapshot.val();
 
-
     if (groupData) {
       return groupData;
-    }else {
-      throw new HttpException("not found", HttpStatus.NOT_FOUND);
+    } else {
+      throw new HttpException("Group not found", HttpStatus.NOT_FOUND);
     }
-
   }
 
+  /**
+   * Create a new group
+   * @param dto - Group data
+   * @param userId - ID of the user creating the group
+   * @returns The created group data
+   */
   async createGroup(dto: CreateGroupDto, userId: number) {
     const user = await this.userRepository.findOneById(userId);
 
@@ -54,7 +64,7 @@ export class ChatService {
 
     groupData.users.push(user);
 
-    // Додайте початкове повідомлення до групи
+    // Add initial message to the group
     const initialMessage = {
       id: user.id.toString(),
       message: 'Welcome to the group!',
@@ -73,23 +83,23 @@ export class ChatService {
     return groupData;
   }
 
+  /**
+   * Send a message to a group
+   * @param dto - Message data
+   * @param id - ID of the user sending the message
+   * @returns The sent message if successful, otherwise returns null
+   */
   async sendMessage(dto: SendMessageDto, id: number) {
     const chat = await this.findGroupById(dto.chatId);
     const user = await this.userRepository.findOneById(id);
 
-      if (user.groups === null) {
-        throw new HttpException("not found", HttpStatus.NOT_FOUND);
-      }
-
-      const guard = user.groups.find(i => i === dto.chatId)
-
-      if (!guard){
-        throw new HttpException("not found", HttpStatus.NOT_FOUND);
-      }
-
-
     if (!user) {
-      throw new HttpException("not found", HttpStatus.NOT_FOUND);
+      throw new HttpException("User not found", HttpStatus.NOT_FOUND);
+    }
+
+    // Check if user is a member of the group
+    if (user.groups === null || !user.groups.includes(dto.chatId)) {
+      throw new HttpException("User is not a member of the group", HttpStatus.NOT_FOUND);
     }
 
     if (chat && Array.isArray(chat.messages)) {
@@ -102,39 +112,43 @@ export class ChatService {
 
       chat.messages.push(newMessage);
 
-      // Оновіть групу з новим повідомленням в базі даних
+      // Update the group with the new message in the database
       const groupRef = ref(this.database, `groups/${dto.chatId}`);
       await set(groupRef, chat);
 
       return newMessage;
     }
-
-    return null;
   }
 
-  async addUserToGroup(dto: AddUserToGroupDto) {
-    const chat = await this.findGroupById(dto.groupId);
-    const user = await this.userRepository.findOne({ where: { id: dto.userId }, });
+    /**
+     * Add a user to a group
+     * @param dto - User and group data
+     * @returns true if the user was added successfully, otherwise throws a NotFoundException or BadRequestException
+     */
 
-    if (!chat || !user) {
-      throw new NotFoundException('Групу або користувача не знайдено');
+    async addUserToGroup(dto: AddUserToGroupDto){
+      const chat = await this.findGroupById(dto.groupId);
+      const user = await this.userRepository.findOne({ where: { id: dto.userId } });
+
+      if (!chat || !user) {
+        throw new NotFoundException('Group or user not found');
+      }
+
+      // Check if the user is already added to the group
+      const isUserInGroup = chat.users.some(u => u.id === user.id);
+      if (isUserInGroup) {
+        throw new BadRequestException('User is already added to the group');
+      }
+
+      chat.users.push(user);
+      user.groups.push(dto.groupId);
+
+      await this.userRepository.save(user);
+
+      // Update the group with the updated users in the database
+      const groupRef = ref(this.database, `groups/${dto.groupId}`);
+      await set(groupRef, chat);
+
+      return true;
     }
-
-    // Перевірка, чи користувач вже доданий до групи
-    const isUserInGroup = chat.users.some(u => u.id === user.id);
-    if (isUserInGroup) {
-      throw new BadRequestException('Користувач вже доданий до групи');
-    }
-
-    chat.users.push(user);
-    user.groups.push(dto.groupId);
-
-    await this.userRepository.save(user);
-
-    // Оновіть групу з оновленими користувачами в базі даних
-    const groupRef = ref(this.database, `groups/${dto.groupId}`);
-    await set(groupRef, chat);
-
-    return true;
-  }
 }
